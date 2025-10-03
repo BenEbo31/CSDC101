b16c2f1cf78050213d81216ce0e3896c71cb0ce8
BenEbo31 <beborde@gbox.adnu.edu.ph>
2025-10-02 22:38:30 -0700

    Create Triangle1.cpp

---
+++ Triangle/Triangle1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+    int main(){
+        
+        int height;
+        
+            cout << "Enter height pero row: ";
+            cin >> height;
+        
+        for (int i = 0; i <= height; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "* ";
+        }
+        
+            cout << endl;
+        }
+        
+        return 0;
+    }
